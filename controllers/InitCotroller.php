a417a6b1d433f9cff71a2bd925b8914102db0400
MarisVigulis <maris.vigulis@inbox.lv>
2015-04-30 15:32:40 +0300

    Init controller added

---
+++ controllers/InitCotroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+	class InitController extends Controller
+	{
+		
+		public function actionIndex()
+		{
+
+
+
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
