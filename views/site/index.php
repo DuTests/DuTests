5d2ccce34e8a1028b57fb9b03c111de3de4abe7f
edgars0507 <edgars0507@inbox.lv>
2015-05-02 21:43:14 +0300

    Added icons

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,31 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+
 ?>
+<html>
+
+<head>
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+<style>
+    .zoom_icon i{
+    margin:20px;
+
+    -moz-transition:-moz-transform 0.5s ease-in; 
+    -webkit-transition:-webkit-transform 0.5s ease-in; 
+    -o-transition:-o-transform 0.5s ease-in;
+    }
+    .zoom_icon i:hover{
+    -moz-transform:scale(2); 
+    -webkit-transform:scale(2);
+    -o-transform:scale(2);
+    }
+</style>
 <div class="site-index">
 
     <div class="jumbotron">
@@ -16,14 +40,15 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <h2>Menu</h2>
+                <p>
+                    <div class="zoom_icon" >
+                        <i class="fa fa-book fa-3x" ></i>
+                    </div>
+                    <div class="zoom_icon" >
+                        <i class="fa fa-graduation-cap fa-3x" ></i>
+                    </div>
+                </p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
@@ -35,16 +60,7 @@ $this->title = 'My Yii Application';
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            
         </div>
 
     </div>
