c249b82d9ed2a3c2168ab4c31cf214b427ad9cef
Artjoms Savostjanovs <qno.public@gmail.com>
2015-05-04 12:41:31 +0300

    userForm view changed

--- models/UserForm.php
+++ models/UserForm.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use Yii;
 use yii\base\Model;
 
 class UserForm extends Model
--- views/site/userForm.php
+++ views/site/userForm.php
@@ -5,9 +5,3 @@ use yii\widgets\ActiveForm;
 
 ?>
 
-<?php $form = ActiveForm::begin(); ?>
-
-<?= $form->field($model, 'name'); ?>
-<?= $form->field($model, 'email'); ?>
-
-<?= Html::submitButton('Submit', ['class'=>'btn btn-success']); ?>
