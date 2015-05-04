1d4dd9f70c5cc94dcc91526d63cf4ee2372f342a
edgars0507 <edgars0507@inbox.lv>
2015-05-04 15:05:44 +0300

    Edited main page

--- views/site/index.php
+++ views/site/index.php
@@ -12,26 +12,10 @@
 
 <body>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
-
-        
-            
-                
-                    <div class="zoom_icon" align="center">
-                        <a href="tests/index"><i class="fa fa-book fa-4x" ></br>Tests</i></a><i class="fa fa-graduation-cap fa-4x" ></br>Results</i>
-                    </div>
-                
-           
-       
-
+    </br>
+         <div class="zoom_icon" align="center" >
+            <a href="tests/index"><i class="fa fa-book fa-4x" ></br>Tests</i></a><i class="fa fa-graduation-cap fa-4x" ></br>Results</i>
+        </div>
     </div>
 </div>
