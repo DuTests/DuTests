2ee338212a3aede0f0e720352b003834b10eb386
MarisVigulis <maris.vigulis@inbox.lv>
2015-05-05 01:02:25 +0300

    Login with Database.OK

--- controllers/InitController.php
+++ controllers/InitController.php
@@ -25,7 +25,8 @@ use app\models\Users;
 				$user[0]->username = "JaOz22";
 				$user[0]->email = "email@email.com";
 				$user[0]->age = 25;
-				$user[0]->password = md5("qwerty");
+				//$user[0]->password = md5("qwerty");
+				$user[0]->password = Yii::$app->security->generatePasswordHash("qwerty");
 				$user[0]->save();
 
 				$user[1]->name = "Peteris";
@@ -33,7 +34,8 @@ use app\models\Users;
 				$user[1]->username = "PeKl20";
 				$user[1]->email = "peteris@mail.ru";
 				$user[1]->age = 20;
-				$user[1]->password = md5("qwerty");
+				//$user[1]->password = md5("qwerty");
+				$user[1]->password = Yii::$app->security->generatePasswordHash("qwerty");
 				$user[1]->save();
 
 				$user[2]->name = "Ilze";
@@ -41,7 +43,8 @@ use app\models\Users;
 				$user[2]->username = "IlPu";
 				$user[2]->email = "ilze@mail.ru";
 				$user[2]->age = 19;
-				$user[2]->password = md5("qwerty");
+				//$user[2]->password = md5("qwerty");
+				$user[2]->password = Yii::$app->security->generatePasswordHash("qwerty");
 				$user[2]->save();
 
 				$user[3]->name = "Anna";
@@ -49,7 +52,8 @@ use app\models\Users;
 				$user[3]->username = "AnZa";
 				$user[3]->email = "anna@mail.cn";
 				$user[3]->age = 20;
-				$user[3]->password = md5("qwerty");
+				//$user[3]->password = md5("qwerty");
+				$user[3]->password = Yii::$app->security->generatePasswordHash("qwerty");
 				$user[3]->save();
 	
 		}
