126f2cfbe511d57fc5997c286a0798e9f004489d
lollyp0p <v.baranovska93@gmail.com>
2015-05-12 23:55:29 +0300

    footer changed

---
+++ views/tests/footer.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<div style='text-align:right '<p>DU Test"."  Current year: ".date("Y")."</p>"."</div>";
+
+?>
--- views/tests/index.php
+++ views/tests/index.php
@@ -1,4 +1,5 @@
 <?php
 use yii\helpers\Html;
+include 'footer.php';
 ?>
 <?= Html::encode($message) ?>
