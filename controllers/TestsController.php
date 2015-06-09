8eb333b5ea8314a167fecb9632f74be0be8a4529
Artyom Savostyanov <asavostyanov@entrypoint.lv>
2015-06-09 22:03:10 +0300

    merge

--- controllers/TestsController.php
+++ controllers/TestsController.php
@@ -14,6 +14,7 @@ use yii\filters\VerbFilter;
  */
 class TestsController extends Controller
 {
+
 	public function actionIndex($msg = "Hello this is testing of 'Tests' controller.")
 	{
 		return $this->render('index', ['message' => $msg]);
@@ -130,6 +131,56 @@ class TestsController extends Controller
         return $this->render('view', [
             'model' => $this->findModel($id),
         ]);
+    }
+
+    /**
+     * Creates a new Tests model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Tests();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->testsid]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Tests model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->testsid]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Tests model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
     }
 
     /**
