605190a29758acd5f4b1de97b8129c5866046ace
edgars0507 <edgars0507@inbox.lv>
2015-06-14 16:41:28 +0300

    edited view

--- views/site/about.php
+++ views/site/about.php
@@ -1,7 +1,8 @@
 <?php
 use yii\helpers\Html;
-
 /* @var $this yii\web\View */
+use app\models\About;
+
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -11,6 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
-
+    Users count: <?=$userscount?></br>
+    Tests count: <?=$testscount?></br>
     <code><?= __FILE__ ?></code>
 </div>
